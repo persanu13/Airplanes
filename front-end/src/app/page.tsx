8ac837ca0bdba8631f9b388dfd2f0fbd265a2619
Stamate Teodor <stamateteodor23@gmail.com>
2024-11-15 10:34:07 +0200

    add front-end

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Image
+        className={styles.title}
+        src={"/assets/title.png"}
+        alt="title"
+        width={408}
+        height={100}
+        priority={true}
+      />
+      <div className={styles.bottomContainer}>
+        <button className="basicButton">
+          <Image
+            src={"/assets/play.png"}
+            alt="border"
+            width={300}
+            height={60}
+          ></Image>
+        </button>
+        <button className="basicButton">
+          <Image
+            src={"/assets/with_a_friend.png"}
+            alt="border"
+            width={300}
+            height={60}
+          ></Image>
+        </button>
+      </div>
+    </div>
+  );
+}
